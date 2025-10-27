546fceabe279fb647806445f17d35a73dc846877
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-10-27 19:29:18 +0100

    updated

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    published_date = models.IntegerField()
 
     def __repr__(self):
         return f"({self.title}, {self.author}, {self.publication_year})"
