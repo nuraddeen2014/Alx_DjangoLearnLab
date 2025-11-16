f0d495627d456b9193a337b4da2209224ff27a92
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-11-16 12:25:06 +0100

    blah blah  blah

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -40,3 +40,4 @@ for b in books_in_library:
 print("\n📌 Librarian of Central Library:")
 print(library1.librarian.name)
 "Library.objects.get(name=library_name)"
+x = "Author.objects.get(name=author_name)", "objects.filter(author=author)"
