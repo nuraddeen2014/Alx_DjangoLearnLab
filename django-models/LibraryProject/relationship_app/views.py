8aeab84aa665c240118f99292684d63cbcc20165
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-11-16 17:52:20 +0100

    create User creation and authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -25,6 +25,6 @@ class LibraryDetailView(DetailView):
         return context
 
 class register(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
