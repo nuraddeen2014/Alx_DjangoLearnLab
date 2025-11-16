b4eafd30f8ecaed7e6a18d4e2b00542d4e1478ab
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-11-16 17:51:33 +0100

    create User creation and authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('register/', register.as_view(), name='register'),
+    path('register/', views.register.as_view(), name='register'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('book_list/', views.book_list, name='book_list'),
