6ab8fa5750c4b51f87679ef00441ccc6010708e6
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-11-16 16:53:43 +0100

    Create Library

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
     
@@ -18,6 +21,11 @@ class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return self.name
+
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
 
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    context = {'books':books}
+
+    return render(request, 'relationship_app/list_books.html', context)
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.get_object().books.all()
+        return context
