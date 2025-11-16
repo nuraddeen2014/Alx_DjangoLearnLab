cd9fff4bb1efacabc667a85494e1c5c292f7db98
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-11-16 17:49:15 +0100

    create User creation and authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -27,4 +28,3 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
-    
