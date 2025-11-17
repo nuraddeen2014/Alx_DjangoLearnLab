737fe0e0ff40b5b11168ab700a7a05ed76023638
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-11-17 12:00:47 +0100

    I pray this works

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book, Library
 from django.views.generic.detail import DetailView
 from .forms import BookForm, RegisterForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
