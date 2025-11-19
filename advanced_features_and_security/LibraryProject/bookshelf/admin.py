1dffcb3752a05a842e772fa785a319c26bea3a64
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-11-19 15:39:13 +0100

    register model in admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .models import *
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {
+            "fields": ("date_of_birth", "profile_photo")
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {
+            "fields": ("date_of_birth", "profile_photo")
+        }),
+    )
+
 
 # Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
+admin.site.register(Library)
+admin.site.register(UserProfile)
+admin.site.register(Book)
