56d98805846c27c7afa358b33ba549ef8ed4a716
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-11-27 03:02:42 +0100

    configure viewsets

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+
+router.register(r'books_all', BookViewSet, basename='books_all')
 urlpatterns = [
-    path('books/', BookList.as_view())
+    path('books/', BookList.as_view()),
+    path('', include(router.urls))
 ]
