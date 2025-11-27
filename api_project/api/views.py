cf2a5a2386e88c8343e4fc93f1988876f0112ca1
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-11-27 10:32:35 +0100

    Add token authentication and permissions to api endpoint

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,12 +3,21 @@ from .models import Book
 from rest_framework import generics
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
 class BookList(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAdminUser, IsAuthenticated]
+
+    
+        
