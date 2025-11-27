18babcd24f83b7ae90ea76f63dcc669d84d12d64
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-11-27 02:39:35 +0100

    serialize the book model, configure url patterns and views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Book
+from rest_framework.generics import ListAPIView
+from .serializers import BookSerializer
 # Create your views here.
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
