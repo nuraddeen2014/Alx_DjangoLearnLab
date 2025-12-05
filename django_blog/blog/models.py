acbfbc1e11bd576b23ae68967e96070eccb8f69d
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-12-05 11:42:41 +0100

    setup blog app settings and necesssary model

---
+++ django_blog/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
