cb6b59ed9914ec67021451c9fcadd04f7ba1c7a3
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-12-10 14:50:10 +0100

    comment model created

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -20,3 +20,12 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
