955f91f31092166cbf8510b69af2d7175e773754
Abdulmalik Haruna Muhammad <princeabdulaliyu@gmail.com>
2025-12-23 10:50:02 +0100

    create endpoint to access feeds and configure it in views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .permissions import OnlyOwnerDeletesPermission
+from django.contrib.auth import get_user_model
 from rest_framework.permissions import (
     IsAuthenticated,
     
@@ -19,6 +20,7 @@ from .serializers import (
     CommentSerializer,
 )
 
+User = get_user_model()
 # Create your views here.
 class PostAPIView(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication, SessionAuthentication]
@@ -38,3 +40,15 @@ class CommentAPIView(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+class FeedViewSet(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticated, OnlyOwnerDeletesPermission]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        # Fetch posts where the author is someone the current user follows
+        # select_related('author') avoids N+1 queries for the author's details
+        return Post.objects.filter(
+            author__in=user.following.all()
+        ).select_related('author')
